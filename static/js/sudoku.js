a0b634c4182cc70e06f86a3940b478f5872a6010
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-02-12 21:44:22 +0200

    Update

--- static/js/sudoku.js
+++ static/js/sudoku.js
@@ -6,10 +6,31 @@ document.querySelector("#dark-mode-toggle").addEventListener("click", () => {
   document.querySelector('meta[name = "theme-color"').setAttribute("content", isDarkMode ? "#1a1a2e" : "#fff");
 });
 
+const name_input = document.querySelector("#input-name");
+const start_screen = document.querySelector("#start-screen");
+
+document.querySelector("#btn-play").addEventListener("click", () => {
+  if (name_input.value.trim().length > 0) {
+    alert("start game");
+  } else {
+    name_input.classList.add("input-err");
+    setTimeout(() => {
+      name_input.classList.remove("input-err");
+      name_input.focus();
+    }, 500);
+  }
+});
+
+const gateGameInfo = () => JSON.parse(localStorage.getItem("game"));
+
 const init = () => {
   const darkMode = JSON.parse(localStorage.getItem("darkmode"));
   document.body.classList.add(darkMode ? "dark" : "light");
   document.querySelector('meta[name = "theme-color"').setAttribute("content", darkMode ? "#1a1a2e" : "#fff");
+
+  const game = gateGameInfo();
+
+  document.querySelector("btn-continue").style.display = game ? "grid" : "none";
 };
 
 init();
